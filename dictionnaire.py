661eb5c0c7dc91ba81fb5e1e4b2844922008e5b9
Maxime BIDEAU <m.bideau@workstation19.nsi.lan>
2025-02-28 13:29:03 +0100

    Fonction trier ajouter

--- dictionnaire.py
+++ dictionnaire.py
@@ -13,7 +13,6 @@ def recupere_donnes():
         # recuperation des données depuis l'url
         url = "https://geobretagne.fr/geoserver/cd29/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=cd29%3Acolleges_29&OUTPUTFORMAT=csv"
         data = requests.get(url).content # on récupère les données sous forme de bytes
-        print("telecarger")
         # création d'un fichier et écriture des données
         with open(local_file, 'wb') as csvfile: # 'w' pour write et 'b' pour bytes
             csvfile.write(data)
@@ -28,5 +27,34 @@ def recupere_donnes():
         
 etablissements = recupere_donnes()
 
-print(etablissements[0:10])
 
+def filtrer(donnes, criteres):
+    result = []
+    compteur = 0
+    for ecole in donnes :
+        for critere in criteres.keys() :
+            if ecole[critere] == criteres[critere] :
+                compteur += 1
+        if compteur == len(criteres) :
+            result.append(ecole['NOM_ET'])
+        compteur = 0
+    return result
+
+#print(filtrer(etablissements, {'INSEE_COMM': '29232', 'STATUT' : 'Privé'}))
+
+def trier(donnees, criteres):
+    """
+    donnees (liste) : une liste de colleges
+    critères (liste) : la liste des colonnes à trier
+    Retourne la liste triée des colleges.
+    """
+    def les_criteres(p):
+        result = []
+        for critere in criteres:
+            result.append(p[critere])
+
+        return result
+
+    return sorted(donnees, key=les_criteres)
+
+print(trier(etablissements, ['STATUT', 'NOM_ET']))
